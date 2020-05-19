610c87c8fbfc03d1a3b7683ab21d615a99d141a5
Solli Honorio <shonorio@gmail.com>
2020-05-19 22:17:16 +0200

    created gradiente background

--- onboarding/lib/app/modules/home/home_page.dart
+++ onboarding/lib/app/modules/home/home_page.dart
@@ -12,5 +12,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends ModularState<HomePage, HomeController> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.1, 0.4, 0.7, 0.9],
+            colors: [
+              Color(0xFF3594DD),
+              Color(0xFF4563DB),
+              Color(0xFF5036D5),
+              Color(0xFF5B16D0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
