c939aafab85fac9ee42ef64a0c47eff8827a6c0f
Solli Honorio <shonorio@gmail.com>
2020-05-19 23:52:01 +0200

    PageView

--- onboarding/lib/app/modules/home/home_page.dart
+++ onboarding/lib/app/modules/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:onboarding/app/modules/home/styles.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +13,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends ModularState<HomePage, HomeController> {
+  final _totalPages = 3;
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,11 +48,190 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
                           style:
                               TextStyle(color: Colors.white, fontSize: 20.0))),
                 ),
+                Container(
+                  height: 600.0,
+                  child: PageView(
+                    physics: ClampingScrollPhysics(),
+                    controller: _pageController,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Center(
+                              child: Image(
+                                image:
+                                    AssetImage('assets/images/onboarding0.png'),
+                                height: 300.0,
+                                width: 300.0,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Text(
+                              'Connect people\naround the world',
+                              style: kTitleStyle,
+                            ),
+                            SizedBox(height: 15.0),
+                            Text(
+                              'Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque pharetra.',
+                              style: kSubtitleStyle,
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Center(
+                              child: Image(
+                                image:
+                                    AssetImage('assets/images/onboarding1.png'),
+                                height: 300.0,
+                                width: 300.0,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Text(
+                              'Live your life smarter with us!',
+                              style: kTitleStyle,
+                            ),
+                            SizedBox(height: 15.0),
+                            Text(
+                              'Sapien metus, scelerisque nec and pharetra id, tempor a tortor. Pellentesque non dignissim neque.',
+                              style: kSubtitleStyle,
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Center(
+                              child: Image(
+                                image:
+                                    AssetImage('assets/images/onboarding2.png'),
+                                height: 300.0,
+                                width: 300.0,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 30.0,
+                            ),
+                            Text(
+                              'Get a new experience of imagination',
+                              style: kTitleStyle,
+                            ),
+                            SizedBox(height: 15.0),
+                            Text(
+                              'Donec facilisis tortor ut augue lacinia, at viverra est semper.',
+                              style: kSubtitleStyle,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: _buildPageIndicator(),
+                ),
+                _currentPage != _totalPages - 1
+                    ? Expanded(
+                        child: Align(
+                          alignment: FractionalOffset.bottomRight,
+                          child: FlatButton(
+                            onPressed: () {
+                              _pageController.nextPage(
+                                duration: Duration(milliseconds: 500),
+                                curve: Curves.ease,
+                              );
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(
+                                  'Next',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22.0,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                  size: 30.0,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    : Container()
               ],
             ),
           ),
         ),
       ),
+      bottomSheet: _currentPage == _totalPages - 1
+          ? Container(
+              height: 100.0,
+              width: double.infinity,
+              color: Colors.white,
+              child: GestureDetector(
+                onTap: () => print('Get started'),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 30.0),
+                    child: Text(
+                      'Get started',
+                      style: TextStyle(
+                          color: Color(0xFF5B16D0),
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : Text(''),
+    );
+  }
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+    for (int i = 0; i < _totalPages; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 150),
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      height: 8.0,
+      width: isActive ? 24.0 : 16.0,
+      decoration: BoxDecoration(
+          color: isActive ? Colors.white : Color(0xFF7B51D3),
+          borderRadius: BorderRadius.all(Radius.circular(12))),
     );
   }
 }
